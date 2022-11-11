0e746dd28799ad612d52ab8cf15034f74ea4fc18
janika.kalliokoski@helsinki.fi <jbkallio@cubbli20-gold.local>
2022-11-11 15:00:12 +0200

    fixing

--- src/varasto.py
+++ src/varasto.py
@@ -1,8 +1,7 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-
-        self.tilavuus = abs(tilavuus)
-
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0
+        # virheellinen, nollataan
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
@@ -14,6 +13,9 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
+
+    # huom: ominaisuus voidaan myös laskea.
+    # Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
 
