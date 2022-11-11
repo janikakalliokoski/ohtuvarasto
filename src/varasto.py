ff1413e2ccc298f48ed0f60ae6bcde9765f1c840
janika.kalliokoski@helsinki.fi <jbkallio@cubbli20-gold.local>
2022-11-11 15:21:18 +0200

    working??

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        self.tilavuus = tilavuus if tilavuus > 0.0 else 0
+        self.tilavuus = abs(tilavuus)
         # virheellinen, nollataan
 
         if alku_saldo < 0.0:
